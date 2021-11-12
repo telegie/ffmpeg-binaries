cee1319659bedc57851e82057a9d13d6d9f251fd
Hanseul Jun <hanseuljun@gmail.com>
2021-11-12 10:54:40 -0800

    Add binaries for emscripten.

--- README.md
+++ README.md
@@ -22,3 +22,16 @@ https://trac.ffmpeg.org/wiki/CompilationGuide/MinGW
 
 
 bin/libiconv-2.dll is from mingw64/bin. It is needed for using ffmpeg binaires built by mingw64.
+
+
+## WASM using a mac
+
+reference: https://itnext.io/build-ffmpeg-webassembly-version-ffmpeg-js-part-2-compile-with-emscripten-4c581e8c9a16
+
+Install emscripten via source (not brew) following https://emscripten.org/docs/getting_started/downloads.html.
+
+Add $EMSDK_ROOT/upstream/bin to path for llvm-ranlib, llvm-as, llvm-nm
+
+Run scripts/configure-wasm.sh
+
+emmake make -j8
---
+++ scripts/configure-wasm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# from: https://itnext.io/build-ffmpeg-webassembly-version-ffmpeg-js-part-2-compile-with-emscripten-4c581e8c9a16
+
+CFLAGS="-s USE_PTHREADS"
+LDFLAGS="$CFLAGS -s INITIAL_MEMORY=33554432"
+
+emconfigure ../FFmpeg/configure \
+  --target-os=none \
+  --arch=x86_32 \
+  --enable-cross-compile \
+  --disable-x86asm \
+  --disable-inline-asm \
+  --disable-stripping \
+  --disable-programs \
+  --disable-doc \
+  --extra-cflags="$CFLAGS" \
+  --extra-cxxflags="$CFLAGS" \
+  --extra-ldflags="$LDFLAGS" \
+  --nm="llvm-nm" \
+  --ar=emar \
+  --ranlib=emranlib \
+  --cc=emcc \
+  --cxx=em++ \
+  --objcc=emcc \
+  --dep-cc=emcc \
+  --prefix=../install
