fc5f4f0acda9cfca5f55d7a35ec24f239a139751
Hanseul Jun <hanseuljun@gmail.com>
2021-11-18 11:41:44 -0800

    Rebuild for windows.

--- scripts/configure-mingw64.sh
+++ scripts/configure-mingw64.sh
@@ -2,9 +2,6 @@
 
 export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/c/Users/hanseul/repos/telegie/deps/ffmpeg-binaries/opus-binaries/e4d4b74/x86_64-w64-mingw32/lib/pkgconfig"
 
-# libssp is required due to opus using fortified functions, which are not included in mingw by default.
-# libssp provides the fortified functions.
-
 ../FFmpeg/configure \
   --target-os=mingw64 \
   --arch=x86_64 \
@@ -16,13 +13,12 @@ export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/c/Users/hanseul/repos/telegie/deps/ffm
   --disable-bzlib \
   --disable-iconv \
   --disable-lzma \
-  --disable-zlib \
-  --disable-encoders \
-  --disable-decoders \
   --enable-libvpx \
   --enable-libopus \
   --enable-encoder=libvpx_vp8,libvpx_vp9,libopus \
   --enable-decoder=vp8,vp9,libopus \
+  --disable-encoder=opus \
+  --disable-decoder=libvpx_vp8,libvpx_vp9,opus \
   --extra-cflags='-I /c/Users/hanseul/repos/telegie/deps/ffmpeg-binaries/libvpx-binaries/1.10.0/x86_64-win64-gcc/include' \
-  --extra-ldflags='-L /c/Users/hanseul/repos/telegie/deps/ffmpeg-binaries/libvpx-binaries/1.10.0/x86_64-win64-gcc/lib -lssp' \
+  --extra-ldflags='-L /c/Users/hanseul/repos/telegie/deps/ffmpeg-binaries/libvpx-binaries/1.10.0/x86_64-win64-gcc/lib' \
   --prefix=../install
