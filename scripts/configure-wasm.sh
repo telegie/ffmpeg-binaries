76c666519a2fd55b161ec30d6ef2a3403d56cf40
Hanseul Jun <hanseuljun@gmail.com>
2021-11-18 20:55:59 -0800

    Add wasm32-emscripten build including opus.

--- scripts/configure-wasm.sh
+++ scripts/configure-wasm.sh
@@ -1,23 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
 # reference: https://itnext.io/build-ffmpeg-webassembly-version-ffmpeg-js-part-2-compile-with-emscripten-4c581e8c9a16
 
+
+
 CFLAGS="-s USE_PTHREADS"
 LDFLAGS="$CFLAGS -s INITIAL_MEMORY=33554432"
+# PKG_CONFIG_PATH as an environment needs to be set this way. Probably because of emconfigure.
+PKG_CONFIG_PATH="/Users/hanseuljun/repos/telegie/deps/ffmpeg-binaries/opus-binaries/e4d4b74/wasm32-emscripten/lib/pkgconfig"
 
 # Using --disable-everything to reduce the byte size of the resulting library.
 emconfigure ../FFmpeg/configure \
   --target-os=none \
   --arch=x86_32 \
   --enable-cross-compile \
-  --disable-x86asm \
-  --disable-inline-asm \
-  --disable-stripping \
-  --disable-programs \
-  --disable-doc \
-  --extra-cflags="$CFLAGS" \
-  --extra-cxxflags="$CFLAGS" \
-  --extra-ldflags="$LDFLAGS" \
   --nm="llvm-nm" \
   --ar=emar \
   --ranlib=emranlib \
@@ -25,7 +21,17 @@ emconfigure ../FFmpeg/configure \
   --cxx=em++ \
   --objcc=emcc \
   --dep-cc=emcc \
+  --disable-x86asm \
+  --disable-inline-asm \
+  --disable-stripping \
+  --disable-programs \
+  --disable-doc \
+  --enable-libopus \
   --disable-everything \
-  --enable-decoder=vp8 \
-  --enable-parser=vp8 \
+  --enable-decoder=vp8,libopus \
+  --enable-parser=vp8,opus \
+  --extra-cflags="$CFLAGS" \
+  --extra-cxxflags="$CFLAGS" \
+  --extra-ldflags="$LDFLAGS" \
+  --env="PKG_CONFIG_PATH=$PKG_CONFIG_PATH" \
   --prefix=../install
