7e44c90e032ea6144fd6f445c281f322c7b14641
Hanseul Jun <hanseuljun@gmail.com>
2021-11-19 12:08:21 -0800

    Disable multithreading from wasm build for ios safari support.

--- scripts/configure-wasm.sh
+++ scripts/configure-wasm.sh
@@ -2,14 +2,12 @@
 
 # reference: https://itnext.io/build-ffmpeg-webassembly-version-ffmpeg-js-part-2-compile-with-emscripten-4c581e8c9a16
 
-
-
-CFLAGS="-s USE_PTHREADS"
 LDFLAGS="$CFLAGS -s INITIAL_MEMORY=33554432"
 # PKG_CONFIG_PATH as an environment needs to be set this way. Probably because of emconfigure.
 PKG_CONFIG_PATH="/Users/hanseuljun/repos/telegie/deps/ffmpeg-binaries/opus-binaries/e4d4b74/wasm32-emscripten/lib/pkgconfig"
 
 # Using --disable-everything to reduce the byte size of the resulting library.
+# --disable-pthreads is due to the current limitation of ios safari.
 emconfigure ../FFmpeg/configure \
   --target-os=none \
   --arch=x86_32 \
@@ -21,6 +19,7 @@ emconfigure ../FFmpeg/configure \
   --cxx=em++ \
   --objcc=emcc \
   --dep-cc=emcc \
+  --disable-pthreads \
   --disable-x86asm \
   --disable-inline-asm \
   --disable-stripping \
@@ -30,8 +29,6 @@ emconfigure ../FFmpeg/configure \
   --disable-everything \
   --enable-decoder=vp8,libopus \
   --enable-parser=vp8,opus \
-  --extra-cflags="$CFLAGS" \
-  --extra-cxxflags="$CFLAGS" \
   --extra-ldflags="$LDFLAGS" \
   --env="PKG_CONFIG_PATH=$PKG_CONFIG_PATH" \
-  --prefix=../install
+  --prefix=../4.4.1/wasm32-emscripten
