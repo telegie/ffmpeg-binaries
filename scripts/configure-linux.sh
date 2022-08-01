97e7da994d3617cf58f4923305ac3bd5c822096c
Hanseul Jun <hanseuljun@gmail.com>
2022-08-01 23:37:18 +0000

    Build with -fPIC flag.

--- scripts/configure-linux.sh
+++ scripts/configure-linux.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$(dirname $(pwd))/opus-binaries/e4d4b74/x64-linux/lib/pkgconfig"
+export PKG_CONFIG_PATH="$(dirname $(pwd))/libvpx-binaries/1.10.0/x64-linux/lib/pkgconfig:$(dirname $(pwd))/opus-binaries/e4d4b74/x64-linux/lib/pkgconfig"
 
-echo "PKG_CONFIG_PATH: ${PKG_CONFIG_PATH}"
-
-pkg-config --exists opus
-echo $?
-
+#reference: https://stackoverflow.com/questions/28884676/error-installing-ffmpeg-on-ubuntu-opus-not-found-using-pkg-config
 ../FFmpeg/configure \
   --target-os=linux \
   --arch=x86_64 \
@@ -20,7 +16,6 @@ echo $?
   --enable-decoder=vp8,vp9,libopus \
   --disable-encoder=opus \
   --disable-decoder=libvpx_vp8,libvpx_vp9,opus \
-  --extra-cflags="-I$(dirname $(pwd))/libvpx-binaries/1.10.0/x64-linux/include" \
-  --extra-ldflags="-L$(dirname $(pwd))/libvpx-binaries/1.10.0/x64-linux/lib" \
-  --pkg-config-flags="--static" \ #reference: https://stackoverflow.com/questions/28884676/error-installing-ffmpeg-on-ubuntu-opus-not-found-using-pkg-config
-  --prefix=../4.4.1/x64-linux
+  --extra-cflags="-fPIC" \
+  --pkg-config-flags="--static" \
+  --prefix="$(dirname $(pwd))/4.4.1/x64-linux"
